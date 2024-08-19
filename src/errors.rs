b3e8fb34a9865098b18a2181cfa8ded2f68dbabc
viktorholk <holkinator@gmail.com>
2024-08-19 14:53:32 +0200

    - Can now add custom startup parameters - Updated config save path - Added support for Linux steam path

--- src/errors.rs
+++ src/errors.rs
@@ -4,12 +4,24 @@ pub type AppResult<T> = std::result::Result<T, AppError>;
 
 #[derive(Error, Debug)]
 pub enum AppError {
+    #[error("Invalid home path")]
+    InvalidHomePath,
+
+    #[error("Invalid path: {0}")]
+    InvalidPath(String),
+
+    #[error("Mod meta.cpp missing or unreadable for mod ID {0}")]
+    MissingMeta(u64),
+
+    #[error("Path conversion error: {0}")]
+    PathConversionError(String),
+
     #[error("IO error: {0}")]
     IoError(#[from] std::io::Error),
 
     #[error("JSON error: {0}")]
     SerdeError(#[from] serde_json::Error),
 
-    #[error("Invalid path: {0}")]
-    InvalidPath(String),
+    #[error("Unsupported platform")]
+    UnsupportedPlatform,
 }
